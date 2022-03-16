01db506a75e4c94f36c3ff7b5adcaf22b3879f82
Rogerio Oliveira <rogeriobsoliveira2006@outlook.com>
2022-03-16 18:51:02 -0300

    Projeto | Ajuste substituição JQuery para JavaScript puro.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+/* $(function(){
   //Abre e fecha menu
   $('.nav-toggle, .nav-close').click(function(e){
     e.preventDefault();
@@ -14,4 +14,28 @@ $(function(){
       $('.header').removeClass('fixed');
     }
   });
-});
+}); */
+
+//Abre e fecha menu com JavaScript
+var navs = document.querySelectorAll('.nav-toggle, .nav-close')
+navs.forEach(item =>{
+  item.addEventListener('click', toggleToClassOnClick)
+})
+
+function toggleToClassOnClick(event) {
+  event.preventDefault()
+  let nav = document.querySelector('nav')
+  nav.classList.toggle('active')
+  console.log(event.target)
+}
+
+//Fixar header com JavaScript
+window.onscroll = function () {
+  let header = document.querySelector('.header')
+  let fixed = header.offsetTop
+  if (window.pageYOffset > fixed) {
+    header.classList.add('fixed')
+  } else {
+    header.classList.remove('fixed')
+  }
+}
